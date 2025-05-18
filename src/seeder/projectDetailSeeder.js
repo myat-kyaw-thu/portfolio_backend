43e6148dac22d4811ca9b6bcfa154e2f5b53dced
Andrew <myatkyawthu4002@gmail.com>
2025-05-18 17:41:49 +0630

    Update projectDetailSeeder.js

--- src/seeder/projectDetailSeeder.js
+++ src/seeder/projectDetailSeeder.js
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
 
 async function main() {
   // Project data array - contains both project details
